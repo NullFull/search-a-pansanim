ce2d5a6c74ad8bfe3f6be88cd22dcb6303e7f0ee
lexifdev <sl@lxf.kr>
2020-12-28 15:48:43 +0900

    static file들 제공

--- anijung/settings.py
+++ anijung/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 #
 GDRIVE_KEY = json.loads(os.environ.get('GDRIVE_KEY', '{}'))
