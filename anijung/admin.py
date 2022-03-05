3238e7cc9f3dd6d0af0e9ff5141052691f62913e
ohahohah <ohahohah.dev@gmail.com>
2022-03-05 20:21:54 +0900

    Add 소속Company 관리페이지

--- anijung/admin.py
+++ anijung/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from nested_inline.admin import NestedStackedInline, NestedModelAdmin
-from .models import Case, Judge, Decision, Quote
+from .models import Case, Judge, Decision, Quote, Company
 
 
 class QuoteInline(NestedStackedInline):
@@ -25,3 +25,4 @@ class JudgeAdmin(NestedModelAdmin):
 admin.site.register(Case, CaseAdmin)
 admin.site.register(Judge, JudgeAdmin)
 admin.site.register(Quote)
+admin.site.register(Company)
