89e72cedbd51e9350a6952257f9de1d60e04ddd3
ohahohah <ohahohah.dev@gmail.com>
2022-03-05 20:22:48 +0900

    Add models 기본 필드 설정

--- anijung/settings.py
+++ anijung/settings.py
@@ -13,7 +13,6 @@ import json
 import os
 import dj_database_url
 
-
 MODE = os.environ.get('MODE', 'develop')
 
 DEBUG = MODE == 'develop'
@@ -32,7 +31,6 @@ if not DEBUG:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -43,7 +41,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', DEFAULT_SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -89,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'anijung.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -115,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -129,7 +124,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
@@ -137,6 +131,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 #
 GDRIVE_KEY = json.loads(os.environ.get('GDRIVE_KEY', '{}'))
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
